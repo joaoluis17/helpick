5be009f7e1b2151fcb1cbf59606668177d60c98d
joaoluis17 <jluispprado@hotmail.com>
2024-10-25 12:35:41 -0300

    arrumando o tailwind do site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js}', // Altere conforme a estrutura do seu projeto
+    './pages/**/*.html',      // Inclui todos os arquivos HTML na pasta 'pages'
+    './parts/**/*.html',      // Inclui todos os arquivos HTML na pasta 'parts'
+    './src/**/*.css',         // Inclui todos os arquivos CSS na pasta 'src'
+    './js/**/*.js',           // Inclui todos os arquivos JavaScript na pasta 'js'
+    './*.html',
   ],
   theme: {
     extend: {},

4502203fe0cd6217b3be74ec0b2053daa25c672c
joaoluis17 <jluispprado@hotmail.com>
2024-10-25 16:55:04 -0300

    passando css para tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ module.exports = {
     './*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brown-eyes': '#77493F', //backgroung do header e footer
+        'darkslateblue': '#483D8BFF', //background do botão
+        'dark-blue': 'rgb(19, 9, 83)', // background hover do botão
+        'gray-300': 'rgba(209, 213, 219, 1)', // Cor do texto
+        'blue-500': 'rgba(96, 165, 250, 0.2)', // Cor da borda
+        'darkbrown': '#2F2325', // cor da linha de separação
+      },
+      fontFamily: {
+        'roboto-flex': ['"Roboto Flex"', 'sans-serif'], // Adiciona a fonte Roboto Flex
+        'tapestry': ['Tapestry', 'cursive'], // Adicionando a fonte 'Tapestry'
+      },
+    },
   },
   plugins: [],
 }

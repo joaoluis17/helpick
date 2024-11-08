4ed632ff50bf5dfc67e42969879780ac9057e32d
ocorvu <professorjoaodamata@gmail.com>
2024-11-08 19:16:50 -0300

    Correção das migrations com campos corretos

--- cordas/database/migrations/0001_01_01_000000_create_users_table.php
+++ cordas/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Adiciona o user_id como chave estrangeira
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
 
--- cordas/database/migrations/2024_11_02_011528_create_comments_table.php
+++ cordas/database/migrations/2024_11_02_011528_create_comments_table.php
@@ -12,7 +12,7 @@ class CreateCommentsTable extends Migration
             Schema::create('comments', function (Blueprint $table) {
                 $table->id();
                 $table->foreignId('post_id')->constrained()->onDelete('cascade');
-                $table->foreignId('user_id')->constrained()->onDelete('cascade');
+                $table->foreignId('user_id')->constrained();
                 $table->text('content');
                 $table->timestamps();
             });
